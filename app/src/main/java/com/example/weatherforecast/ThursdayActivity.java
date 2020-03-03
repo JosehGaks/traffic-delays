d0bc7dc82c1f05faefbab6fb2c92f4d7d0dd0b3e
JosehGaks <josehwilddog@gmail.com>
2020-03-03 13:39:38 +0300

    create a image arraylist in thursday activity

---
+++ app/src/main/java/com/example/weatherforecast/ThursdayActivity.java
@@ -0,0 +1,31 @@
+package com.example.weatherforecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ThursdayActivity extends AppCompatActivity {
+
+    char degrees = '\u00B0';
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_list);
+        ArrayList<Weather> temperature = new ArrayList<Weather>();
+
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.rain_cloud,"12"+degrees,"11"+degrees));
+        temperature.add(new Weather(R.drawable.partly_cloudy_day,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+
+        WeatherAdapter adapter = new WeatherAdapter(this,temperature);
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+    }
+}
