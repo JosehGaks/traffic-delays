0643e4b3a31189313baf39f92dc66d22d2472c5e
JosehGaks <josehwilddog@gmail.com>
2020-03-03 13:36:58 +0300

    create an array list for temperatures in the monday activity

--- app/src/main/java/com/example/weatherforecast/MondayActivity.java
+++ app/src/main/java/com/example/weatherforecast/MondayActivity.java
@@ -3,12 +3,29 @@ package com.example.weatherforecast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MondayActivity extends AppCompatActivity {
-
+   char degrees = '\u00B0';
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monday);
+        setContentView(R.layout.weather_list);
+        ArrayList<Weather> temperature = new ArrayList<Weather>();
+
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.rain_cloud,"12"+degrees,"11"+degrees));
+        temperature.add(new Weather(R.drawable.partly_cloudy_day,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+        temperature.add(new Weather(R.drawable.clouds,"14"+degrees,"15"+degrees));
+
+        WeatherAdapter adapter = new WeatherAdapter(this,temperature);
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
     }
 }
